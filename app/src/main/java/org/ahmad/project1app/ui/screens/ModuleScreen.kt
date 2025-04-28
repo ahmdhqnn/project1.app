35dfe0694fc32a1e639a864c91259f7628f9a685
Ahmad Hakin Najili <ahmadhaqinn@gmail.com>
2025-04-28 22:20:11 +0700

    screen modul

--- app/src/main/java/org/ahmad/project1app/ui/screens/ModuleScreen.kt
+++ app/src/main/java/org/ahmad/project1app/ui/screens/ModuleScreen.kt
@@ -1,14 +1,25 @@
 package org.ahmad.project1app.ui.screens
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ArrowBack
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.Build
+import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -16,11 +27,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,14 +43,11 @@ import org.ahmad.project1app.ui.theme.Project1appTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ModuleScreen(navController: NavHostController){
+fun ModuleScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-
-
-                ),
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
                 title = {
                     Text(
                         text = stringResource(R.string.module_title),
@@ -47,42 +56,74 @@ fun ModuleScreen(navController: NavHostController){
                     )
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = { navController.popBackStack() }) {
                         Icon(
-                            imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
-                            contentDescription = "Localized description"
+                            imageVector = Icons.AutoMirrored.Rounded.KeyboardArrowLeft,
+                            contentDescription = stringResource(R.string.back)
                         )
                     }
                 },
                 actions = {
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
-                            imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description"
+                            imageVector = Icons.Rounded.Menu,
+                            contentDescription = stringResource(R.string.menu)
                         )
                     }
-                },
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {  },
+                shape = CircleShape,
+                containerColor = Color.White,
+                contentColor = Color.Black,
+                modifier = Modifier.offset(y = (-100).dp)
+            ) {
+                Icon(Icons.Rounded.Build, stringResource(R.string.glossary))
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(horizontal = 16.dp)
+                .padding(bottom = 60.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.respiratory_system),
+                contentDescription = stringResource(R.string.respiratory_system_image),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
 
-                )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.module_content_title),
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.module_content_paragraph1),
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.module_content_paragraph2),
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
         }
-    ) {
-            innerPadding -> ScreenContent(Modifier.padding(innerPadding))
-
-    }
-}
-
-@Composable
-private fun ScreenContent(modifier: Modifier ){
-    Box(
-        modifier = Modifier.padding(16.dp).fillMaxHeight(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = stringResource(R.string.module_dummy),
-            style = MaterialTheme.typography.headlineLarge,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
     }
 }
 
